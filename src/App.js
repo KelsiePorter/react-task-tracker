620ddd709e784a2fe76d21194f5a929f09f54613
Kelsie Porter <krporter22@gmail.com>
2023-04-17 14:44:29 -0600

    WIP styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
-function App() {
+
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'May 10th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Project Due',
+      day: 'May 14th at 9:30am',
+      reminder: true,
+    },  {
+      id: 3,
+      text: 'Grocery Shopping',
+      day: 'May 20th at 4:00pm',
+      reminder: false,
+    },
+  ])
+
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => 
+    task.id === id ? { ...task, reminder: !task.reminder } 
+    : task))
+}
+
   return (
-    <div className="container">
-      <h1>Hello from React</h1>
+    <div className='container'>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} 
+      onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks To Show')}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,22 @@
-rafce
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title, onAdd }) => {
+  return (
+    <header className='header'>
+      <h1 >{title}</h1>
+      <Button color='tan' text='Add a Task' onClick=
+      {onAdd} />
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+export default Header
